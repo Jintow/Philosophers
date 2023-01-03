8bb51d17accccd933918fa78a8bd7ae46511e9ac
Johan Litaudon <118103893+Jintow@users.noreply.github.com>
2023-01-03 23:26:11 +0100

    mise en forme du fichier .h

--- headers/philosophers.h
+++ headers/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: Teiki <Teiki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 12:54:00 by Teiki             #+#    #+#             */
-/*   Updated: 2023/01/03 23:20:37 by Teiki            ###   ########.fr       */
+/*   Updated: 2023/01/03 23:25:47 by Teiki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,14 +68,22 @@ typedef struct s_philo
 	pthread_mutex_t	*fork;
 }t_philo;
 
+/*	------------ EXIT FUNCTIONS -------------  */
+
 void	fail_exit(char *msg, char **tab_arg);
 void	fail_exit2(char *msg, t_philo *philo, int nb);
 void	destroy_and_exit(t_philo *philo);
+
+/*	------------ CHECKING FUNCTIONS -------------  */
+
+int		central_checking(char **tab_nb);
 void	init_struct_philo(t_philo *philo, char **tab_info);
+char	**init_args(char **argv);
+
+/*	------------ SIMULATION FUNCTIONS -------------  */
+
 void	activate_simulation(t_philo *philo);
 void	*living(void *arg);
-char	**init_args(char **argv);
-int		central_checking(char **tab_nb);
 int		check_endof_sim(t_philo_id *philo, int death_time);
 int		activity_time(t_philo_id *philo, int time_activity, char c);
 int		eating(t_philo_id *philo);
