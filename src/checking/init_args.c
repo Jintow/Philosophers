f73fa05123b01c16dee49c5edf1e2eea6a72952a
Johan Litaudon <118103893+Jintow@users.noreply.github.com>
2023-01-03 23:32:09 +0100

    modifications mineures dans init_args, supressions des commentaires du main

--- src/checking/init_args.c
+++ src/checking/init_args.c
@@ -6,7 +6,7 @@
 /*   By: Teiki <Teiki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 13:20:19 by Teiki             #+#    #+#             */
-/*   Updated: 2022/12/27 13:56:38 by Teiki            ###   ########.fr       */
+/*   Updated: 2023/01/03 23:30:59 by Teiki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ char	**init_args(char **argv)
 
 	value = ft_calloc(1, sizeof(char));
 	if (!value)
-		exit(EXIT_FAILURE);
+		fail_exit(ERR_MALLOC, NULL);
 	i = -1;
 	while (argv[++i])
 	{
 		value = ft_strjoin_free_s1(value, argv[i]);
 		if (!value)
-			exit(EXIT_FAILURE);
+			fail_exit(ERR_MALLOC, NULL);
 		value = ft_strjoin_free_s1(value, " ");
 		if (!value)
-			exit(EXIT_FAILURE);
+			fail_exit(ERR_MALLOC, NULL);
 	}
 	tab_nb = ft_split(value, ' ');
 	free(value);
 	if (!tab_nb)
-		exit(EXIT_FAILURE);
+		fail_exit(ERR_MALLOC, NULL);
 	return (tab_nb);
 }
